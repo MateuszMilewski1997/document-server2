e487a0ded05587f2f8bd9637c9213df54ece50d0
Rafał Nowociński <vito24222@gmail.com>
2019-05-12 18:32:28 +0200

    add return title to details case

--- document-server2/document-server2.Infrastructure/DTO/CaseDTO.cs
+++ document-server2/document-server2.Infrastructure/DTO/CaseDTO.cs
@@ -5,6 +5,7 @@ namespace document_server2.Infrastructure.DTO
     public class CaseDTO
     {
         public string Id { get; set; }
+        public string Title { get; set; }
         public string User_email { get; set; }
         public string Type { get; set; }
         public DateTime Date { get; set; }
